811dd153bc59e3265c33a60e37de7618fc4ab02c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-11-28 07:08:29 +0530

    added sub-component LogItem.js in Logs.js

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -23,14 +24,14 @@ const Logs = () => {
 
   return (
     <div>
-      <ul className='collection-with-header'>
+      <ul className='collection with-header'>
         <li className='collection-header'>
           <h4 className='center'>System Logs</h4>
         </li>
         {!loading && logs.length === 0 ? (
           <p className='center'>No Logs to show...</p>
         ) : (
-          logs.map((log) => <li>{log.message}</li>)
+          logs.map((log) => <LogItem log={log} key={log.id} />)
         )}
       </ul>
     </div>
