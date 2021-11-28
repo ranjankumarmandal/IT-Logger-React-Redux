c109d0a5093fa0f772b321d2a8d0ffdd5d1c7261
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-11-28 18:58:06 +0530

    created component AddTechModal.js

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const AddTechLogModal = () => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+
+  const onSubmit = () => {
+    if (firstname === '' || lastname === '') {
+      M.toast({ html: 'Please enter a firstname and lastname' });
+    } else {
+      console.log(firstname, lastname);
+
+      // after adding clear fields
+      setFirstname('');
+      setLastname('');
+    }
+  };
+
+  return (
+    <div id='add-tech-modal' className='modal'>
+      <div className='modal-content'>
+        <h4>New Technician</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='firstname'
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+            <label htmlFor='firstname' className='active'>
+              First Name
+            </label>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='lastname'
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+            />
+            <label htmlFor='lastname' className='active'>
+              Last Name
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close waves-effect blue waves-light btn'
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddTechLogModal;
