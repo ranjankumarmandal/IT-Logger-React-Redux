3dac117c1657be931f6b71b0f28b0bfe670b2ee1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-12-02 00:28:34 +0530

    added some support methods in logActions.js

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,8 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
 } from './types';
 
 // get logs from server
@@ -71,6 +73,40 @@ export const deleteLog = (id) => async (dispatch) => {
       payload: error.response.data,
     });
   }
+};
+
+// update log on server
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`http://localhost:5000/logs/${id}`, {
+      method: 'DELETE',
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 export const setLoading = () => {
