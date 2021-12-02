3eaa83e110544be07b987a9007f52ffda7840ef9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-12-02 10:57:03 +0530

    added getTechs() into component TechListModal.js

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getTechs } from '../../actions/techActions';
 import TechItem from './TechItem';
 
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
   useEffect(() => {
     getTechs();
   }, []);
-
-  const getTechs = async () => {
-    setLoading(true);
-    const res = await fetch('http://localhost:5000/techs');
-    const data = await res.json();
-
-    setTechs(data);
-    setLoading(false);
-  };
 
   return (
     <div id='tech-list-modal' className='modal'>
@@ -24,6 +14,7 @@ const TechListModal = () => {
         <h4>Technician List</h4>
         <ul className='collection'>
           {!loading &&
+            techs !== null &&
             techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}
         </ul>
       </div>
@@ -31,4 +22,8 @@ const TechListModal = () => {
   );
 };
 
-export default TechListModal;
+const mapStateToProps = (state) => ({
+  tech: state.tech,
+});
+
+export default connect(mapStateToProps, { getTechs })(TechListModal);
